155f8145ee4aad2395ad707574a1515d7308e260
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-24 02:20:19 +0300

    get job method added

--- database/database.go
+++ database/database.go
@@ -2,11 +2,15 @@ package database
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"time"
+
+	"github.com/Danitilahun/GraphQL_GO_MongoDB.git/graph/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var connectionString string = ""
@@ -31,4 +35,21 @@ func Connect() *DB {
 	return &DB{
 		client: client,
 	}
+}
+
+
+
+func (db *DB) GetJob(id string) *model.JobListing {
+	jobCollec := db.client.Database("graphql-job-board").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	_id, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": _id}
+	var jobListing model.JobListing
+	err := jobCollec.FindOne(ctx, filter).Decode(&jobListing)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &jobListing
 }
