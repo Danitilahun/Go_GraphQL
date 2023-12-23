3aa15b873f31b06a817fe10e683b9bad19ff03d3
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-24 02:09:09 +0300

    Adding mongodb connect function

--- database/database.go
+++ database/database.go
@@ -2,8 +2,6 @@ package database
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -15,4 +13,22 @@ var connectionString string = ""
 
 type DB struct {
 	client *mongo.Client
+}
+
+func Connect() *DB {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &DB{
+		client: client,
+	}
 }
