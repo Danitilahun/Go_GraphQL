605c8248d8fece6b6d98e98d22396f8e28f35d6a
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-24 01:57:04 +0300

    creating database struct

---
+++ database/database.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+var connectionString string = ""
+
+type DB struct {
+	client *mongo.Client
+}
